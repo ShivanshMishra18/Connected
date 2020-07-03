ff7424cfca1bd6e87f1b7120e08695466868d99c
ShivanshMishra18 <shivansh18@iitg.ac.in>
2020-07-03 23:49:02 +0530

    Housekeeping

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -3,71 +3,52 @@ import axios from 'axios'
 import setAuthToken from "../utils/setAuthToken"
 import jwt_decode from 'jwt-decode'
 
+
 // Register User
 export const registerUser = (userData, history) => async dispatch => {
-    
     try {
-        await axios.post('/api/user/register', userData)
-        // console.log(res.data)
+        await axios.post('/api/user/register', userData) // no response required
         history.push('/login')      // redirecting to the login page
-    } catch (err) {
-        // console.log(err.response.data)
-        // this.setState({ errors: err.response.data }) // cannot perfor this here as outside register component
+    } 
+    catch (err) {
+        // this.setState({ errors: err.response.data }) 
+        // cannot perform it here as outside register component
         dispatch({
             type: GET_ERRORS,
             payload: err.response.data
         })
     }
 }
+// Double arrow is used to denote that a function is being returned
+// This is possible with thunk
+// Syntax can used to normally return function as well (like login)
+
 
 // Login - Get Auth Token
-export const loginUser = userData => dispatch => {
-    
-    axios.post('/api/user/login', userData)
-    .then(res => {
-        // Save to local storage
-        const { token } = res.data
-        // Set token to local storage
-        localStorage.setItem('jwtToken', token)
-        // Set token to Auth header
-        setAuthToken(token)
-        // Decode token to get user data
-        const decoded = jwt_decode(token)
-        // Set current user
-        dispatch(setCurrentUser(decoded))}
-    )
-    .catch(err => {
-      dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
-        })
-    })
-}
+export function loginUser(userData){
+    return async function f(dispatch) {
+        try {
+            const res = await axios.post('/api/user/login', userData)
+            // Save to local storage
+            const { token } = res.data
+            // Set token to local storage
+            localStorage.setItem('jwtToken', token)
+            // Set token to Auth header
+            setAuthToken(token)
+            // Decode token to get user data
+            const decoded = jwt_decode(token)
+            // Set current user
+            dispatch(setCurrentUser(decoded))
+        } 
+        catch (err) {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        }
+    }
+} 
 
-// Login - Get Auth Token
-// export function loginUser(userData){
-//     return async function f(dispatch) {
-//         try {
-//             const res = await axios.post('/api/user/login', userData)
-//             // Save to local storage
-//             const { token } = res.data
-//             // Set token to local storage
-//             localStorage.setItem('jwtToken', token)
-//             // Set token to Auth header
-//             setAuthToken(token)
-//             // Decode token to get user data
-//             const decoded = jwt_decode(token)
-//             // Set current user
-//             dispatch(setCurrentUser(decoded))
-//         } 
-//         catch (err) {
-//             dispatch({
-//                 type: GET_ERRORS,
-//                 payload: err.response.data
-//             })
-//         }
-//     }
-// } 
     
 // Set logged in user
 export const setCurrentUser = decoded => {
