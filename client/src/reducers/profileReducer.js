6fea596147e902e61635a145ad751f094c1b6992
ShivanshMishra18 <shivansh18@iitg.ac.in>
2020-07-04 16:31:50 +0530

    Profile state boilerplate

---
+++ client/src/reducers/profileReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    profile: null,
+    profiles: null,
+    loading: false
+}
+
+export default function(state, action) {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
