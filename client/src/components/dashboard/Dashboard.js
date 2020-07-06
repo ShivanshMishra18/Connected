ea1edaf6b549196b1bad7864126d8bd76d9259e2
ShivanshMishra18 <shivansh18@iitg.ac.in>
2020-07-06 20:42:14 +0530

    Buttons for dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,59 +4,69 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { getCurrentProfile } from '../../actions/profileActions'
 import Spinner from '../common/Spinner'
+import ProfileButtons from './ProfileButtons'
  
 class Dashboard extends Component {
     
     componentDidMount() {
-        // TODO : Unauthorized request to reach dashboard
-        // if (!this.props.auth.isAuthenticated) {
-        //     this.props.history.push('/login')
-        // }
-        // However, we will use Private Router to do this
+      // TODO : Unauthorized request to reach dashboard
+      // if (!this.props.auth.isAuthenticated) {
+      //     this.props.history.push('/login')
+      // }
+      // However, we will use Private Router to do this
 
-        this.props.getCurrentProfile()
+      this.props.getCurrentProfile()
     }
     
     render() {
 
-        const { user } = this.props.auth
-        const { profile, loading } = this.props.profile
+      const { user } = this.props.auth
+      const { profile, loading } = this.props.profile
 
-        let dashboardContent
+      let dashboardContent
 
-        if (profile === null || loading) {
-            dashboardContent = <Spinner />
-        } else {
-            // If logged in user has profile
-            if (Object.keys(profile).length) {
-                dashboardContent = <h4>DISPLAY PROFILE</h4>
-            }
-            // If logged in user's profile is empty
-            else {
-                dashboardContent = (
-                    <div>
-                        <p className="lead text-muted">Welcome {user.name}</p>
-                        <p> Your profile is empty, please add some info </p>
-                        <Link to="/create-profile" className="btn btn-lg btn-info">
-                            Create Profile
-                        </Link>
-                    </div>
-                )
-            }
-        }
-
-        return (
-            <div className="dashboard">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <h1 className="display-4">Dashboard</h1>
-                            { dashboardContent }
-                        </div>
-                    </div>
-                </div>                
+      if (profile === null || loading) {
+          dashboardContent = <Spinner />
+      } 
+      else {
+        // If logged in user has profile
+        if (Object.keys(profile).length) {
+          dashboardContent = (
+            <div>
+                <p className="lead text-muted"> Welcome 
+                  <Link to={`/profile/${profile.handle}`}> {user.name} </Link>
+                </p>
+                <ProfileButtons />
             </div>
-        )
+          )
+        }
+        // If logged in user's profile is empty
+        else 
+        {
+          dashboardContent = (
+            <div>
+                <p className="lead text-muted">Welcome {user.name}</p>
+                <p> Your profile is empty, please add some info </p>
+                <Link to="/create-profile" className="btn btn-lg btn-info">
+                    Create Profile
+                </Link>
+            </div>
+          )
+        }
+      }
+
+      return (
+        <div className="dashboard">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                  <h1 className="display-4">Dashboard</h1>
+                  { dashboardContent }
+              </div>
+            </div>
+          </div>                
+        </div>
+      )
     }
 }
 
